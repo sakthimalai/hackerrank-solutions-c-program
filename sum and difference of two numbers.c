f27799ab79f8073c07fbe5b4ee643e085ba6cfb9
sakthimalai <100014885+sakthimalai@users.noreply.github.com>
2022-11-13 21:01:49 +0530

    Add files via upload

---
+++ sum and difference of two numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+ int a,b;
+    float x,y;
+    scanf("%d%d",&a,&b); //input for int datatype
+    scanf("%f%f",&x,&y); //input for float datatype
+    printf("%d %d \n",a+b,a-b);
+    printf("%0.1f %0.1f",x+y,x-y);
+    
+    return 0;
+}
