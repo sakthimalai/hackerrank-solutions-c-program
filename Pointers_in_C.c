f5e152288e1e418a7b5c0c5a503f6b04d0d1b3de
sakthimalai <100014885+sakthimalai@users.noreply.github.com>
2022-11-15 19:14:18 +0530

    Update Pointers_in_C.c

--- Pointers_in_C.c
+++ Pointers_in_C.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 void update(int *a,int *b) {
-    // Complete this function    
      int sum,diff;
      sum = *a+*b;
      diff = abs(*a-*b);
+    /*abs () function is a predefined function in the stdlib.h
+   header file to return the absolute value of the given integers.
+   The absolute value of -35 is 35.*/
      *a = sum;
      *b = diff;
 }
@@ -14,7 +16,7 @@ int main() {
     int *pa = &a, *pb = &b;
     
     scanf("%d %d", &a, &b);
-    update(pa, pb);
+    update(pa, pb);//Function calling
     printf("%d\n%d", a, b);
 
     return 0;
